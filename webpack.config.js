78f152af79bceeaa11f67d04256ceaa8ed7efe75
David Gómez <38304163+DG-Develop@users.noreply.github.com>
2021-10-30 22:16:52 -0500

    Agregando librerias y configurando webpack en un estado inicial

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@actions': path.resolve(__dirname, 'src/actions'),
+            '@assetComponent': path.resolve(__dirname, 'src/assets/components'),
+            '@assetContainer': path.resolve(__dirname, 'src/assets/containers'),
+            '@static': path.resolve(__dirname, 'src/assets/static'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@containers': path.resolve(__dirname, 'src/containers'),
+            '@asset': path.resolve(__dirname, 'src/assets'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|gif|jpg|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
+        }),
+        new Dotenv()
+    ]
+
+}
