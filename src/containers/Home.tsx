fe7013ba487038f98d9ba304e86ccb5dfeef8812
DG-Develop <dgdband@gmail.com>
2022-06-05 21:55:40 -0500

    [ADD] Agregando assets para la pagina principal además de estructurar el header de esta misma

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,31 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return (
-        <>
-            <header>
-                <nav>
-                    <span className='logo'></span>
-                    <li>
-                        <ul>Home</ul>
-                        <ul>About</ul>
-                        <ul>Work</ul>
-                        <ul>Contact</ul>
-                    </li>
-                </nav>
-            </header>
-            <main>
-                <section>
-                    <div>
-                        <h1>Hi, I'm Full Stack Developer</h1>
-                        <p>Specialist in database management, API creation and web application designer.</p>
+  return (
+    <>
+      <header>
+        <nav>
+          <span className="logo"></span>
+          <li>
+            <ul>Home</ul>
+            <ul>About</ul>
+          </li>
+          <li>
+            <ul>Work</ul>
+            <ul>Contact</ul>
+          </li>
+          <li>
+            <ul>
+              <a href="">
+                <span className="twitter"></span>
+              </a>
+            </ul>
+            <ul>
+              <a href="">
+                <span className="github"></span>
+              </a>
+            </ul>
+          </li>
+        </nav>
+      </header>
+      <main>
+        <section>
+          <div>
+            <h1>Hi, I'm Full Stack Developer</h1>
+            <p>
+              Specialist in database management, API creation and web
+              application designer.
+            </p>
 
-                        <button>Go to Work</button>
-                    </div>
-                </section>
-            </main>
-        </>
-    )
-}
+            <button>Go to Work</button>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
 
-export default Home
+export default Home;
