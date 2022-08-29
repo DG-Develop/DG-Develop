eba3961bf4bb534cab556c969c7a292480d74226
DG-Develop <dgdband@gmail.com>
2022-08-28 21:57:12 -0500

    [ADD] Agregando ajustes menores

--- src/components/ModalMenu.jsx
+++ src/components/ModalMenu.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import '../assets/components/ModalMenu.scss'
+import React from "react";
+import "../assets/components/ModalMenu.scss";
 
 const ModalMenu = () => {
   return (
-    <div className='modal'>
-        <div>
+    <div className="modal">
+      <div>
         <li>
-            <ul>Home</ul>
-            <ul>About</ul>
-            <ul>Work</ul>
-            <ul>Contact</ul>
-          </li>
-        </div>
+          <ul>Home</ul>
+          <ul>About</ul>
+          <ul>Work</ul>
+          <ul>Contact</ul>
+        </li>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ModalMenu
+export default ModalMenu;
