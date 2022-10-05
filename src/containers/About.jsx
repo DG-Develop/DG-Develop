35c0f348b3eb2549ad86d9661ba961de142d8839
DG-Develop <dgdband@gmail.com>
2022-10-04 23:24:56 -0500

    [ADD] Agregando la maquetaciön sobre mi al proyecto

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Header } from "../components/Header";
+
+import me from "../assets/static/me.png";
+import meBig from "../assets/static/me_big.png";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <Header />
 
-export default About
+      <section className="about">
+        <div>
+          <h2>This is some about me...</h2>
+          <div className="about__image">
+            <picture>
+              <source srcSet={meBig} media="(min-width: 1024px)" />
+              <img src={me} alt="My photo presentation" />
+            </picture>
+          </div>
+          <div className="about__content">
+            <hr />
+            <p>
+              I'm a Backend Developer with 3 years experience in development and
+              analytics about APIs and integration and deploy application web
+              and mobile.
+              <br />
+              Also UI and UX design with 
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
