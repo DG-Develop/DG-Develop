872f76b798b22fafd4faf07ca040ff97b38efd4d
DG-Develop <dgdband@gmail.com>
2022-10-13 23:23:12 -0500

    [ADD] Agregando static files y reacomodando el layout de la página de work del proyecto

--- src/containers/Work.jsx
+++ src/containers/Work.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import IconAnimated from "../components/IconAnimated";
 import { Header } from "../components/Header";
+import genc from "../assets/static/genc.jpg";
 
 const Work = () => {
   return (
@@ -12,17 +13,32 @@ const Work = () => {
           <IconAnimated />
         </div>
 
-        <div className="scroll-content">
-          <div>
-            <h1>Welcome to my portfolio works</h1>
-            <p>In this section you can see all my projects that I made it</p>
+        <section className="work-start">
+          <div className="scroll-content">
+            <div>
+              <h1>Welcome to my portfolio works</h1>
+              <p>In this section you can see all my projects that I made it</p>
+            </div>
+
+            <div className="scroll-motion">
+              <span className="scroll-icon"></span>
+              <p>Scroll On!</p>
+            </div>
           </div>
 
-          <div className="scroll-motion">
-            <span className="scroll-icon"></span>
-            <p>Scroll On!</p>
-          </div>
-        </div>
+          <section className="work-content">
+            <div>
+              <h3>GenC</h3>
+              <p>Contract generator</p>
+            </div>
+
+            <div>
+              <div>
+                <img src={genc} alt="work image about contract generator" />
+              </div>
+            </div>
+          </section>
+        </section>
       </section>
     </>
   );
