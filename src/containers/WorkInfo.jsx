9eb20ab39c604bb2cd0720ae3f550778bee6dc00
DG-Develop <dgdband@gmail.com>
2022-11-08 23:27:42 -0600

    [ADD] Terminando de agregar los estilos de la página de la información de un trabajo

--- src/containers/WorkInfo.jsx
+++ src/containers/WorkInfo.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import TonalButtonWithIcon from "../widgets/TonalButtonWithIcon";
 
 import gecon from "../assets/static/Gecon.png";
+import { Link } from "react-router-dom";
 
 const WorkInfo = () => {
   return (
     <main className="info__container">
       <section className="info__about">
         <div className="sticky__header">
-          <span className="i_back"></span>
+          <span className="i_back">
+            <Link to="/work"></Link>{" "}
+          </span>
           <h1>GECON</h1>
         </div>
 
         <div className="info__content">
           <p>Contract generator</p>
           <p>
-            Aplicación PWA para generar contratos por tiempo determinado al
-            personal de nómina para el departamento de Recursos Humanos con el
-            fin de tener un control presupuestal y análisis del personal que ha
-            sido contratado por este medio.
+            PWA application for generate fixed-terms contracts for the payroll
+            personal of Human Resources department with the final porpuse we
+            have the presupuestal control and personal analysis that has been
+            hired by this means.
           </p>
         </div>
 
@@ -38,12 +41,44 @@ const WorkInfo = () => {
       </section>
 
       <section className="info__description">
-        <div>
-          <div>
+        <div className="description__work-item">
+          <div className="work-item__header">
             <h4>Home</h4>
             <img src={gecon} alt="image home of proyect GECON" />
           </div>
-          <div>
+          <div className="work-item__content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus sit
+              amet rutrum suspendisse. Libero consequat, ante hac risus.
+              Parturient scelerisque tempor, lobortis vestibulum, sed
+              scelerisque massa tincidunt. Ut quis fermentum vitae volutpat
+              auctor facilisis.
+            </p>
+          </div>
+        </div>
+
+        <div className="description__work-item invert">
+          <div className="work-item__header">
+            <h4>Employee List</h4>
+            <img src={gecon} alt="image home of proyect GECON" />
+          </div>
+          <div className="work-item__content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus sit
+              amet rutrum suspendisse. Libero consequat, ante hac risus.
+              Parturient scelerisque tempor, lobortis vestibulum, sed
+              scelerisque massa tincidunt. Ut quis fermentum vitae volutpat
+              auctor facilisis.
+            </p>
+          </div>
+        </div>
+
+        <div className="description__work-item">
+          <div className="work-item__header">
+            <h4>Contract List</h4>
+            <img src={gecon} alt="image home of proyect GECON" />
+          </div>
+          <div className="work-item__content">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus sit
               amet rutrum suspendisse. Libero consequat, ante hac risus.
