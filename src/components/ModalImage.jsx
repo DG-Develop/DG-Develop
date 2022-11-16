8ed5760e01368f409aa46ce01f5dbbf15467effc
DG-Develop <dgdband@gmail.com>
2022-11-15 23:29:39 -0600

    [ADD] Agregando el modal para ver las imagenes del proyecto

---
+++ src/components/ModalImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Modal from './Modal'
+
+const ModalImage = ({ isOpen, OnCloseModal}) => {
+  return (
+    <Modal isOpen={isOpen}>
+        
+    </Modal>
+  )
+}
+
+export default ModalImage
