c48a20f09ee9cba74e6f5e98160aa095bbe4ad08
DG-Develop <dgdband@gmail.com>
2022-12-04 17:06:37 -0600

    [ADD] Agregando todos los recursos faltantes para el trabajo de Gencon

--- src/features/works/workSlice.ts
+++ src/features/works/workSlice.ts
@@ -8,10 +8,18 @@ const geconMonthly = require("../../assets/static/Gecon_Monthly.png");
 const geconBiweekly = require("../../assets/static/Gecon_Biweekly.png");
 const geconDay = require("../../assets/static/Gecon_Day.png");
 const geconContractList = require("../../assets/static/Gecon_Contract_List.png");
+const geconContractInfo = require("../../assets/static/Gecon_Contract_Info.png");
 const geconEmployeeList = require("../../assets/static/Gecon_Employee_List.png");
+const geconPersonalInfo = require("../../assets/static/Gecon_Personal_Info.png");
+const geconPersonalAddress = require("../../assets/static/Gecon_Personal_Address.png");
 const geconGenerateContracts = require("../../assets/static/Gecon_Generate_Contracts.png");
+const geconGenerateContracts2 = require("../../assets/static/Gecon_Generate_Contracts2.png");
+const geconGenerateContracts3 = require("../../assets/static/Gecon_Generate_Contracts3.png");
 const geconUpdateContract = require("../../assets/static/Gecon_Update_Contract.png");
+const geconUpdateContractDependencia = require("../../assets/static/Gecon_Update_Contract_Dependencia.png");
+const geconUpdateContractPuesto = require("../../assets/static/Gecon_Update_Contract_Puesto.png");
 const geconAddSignature = require("../../assets/static/Gecon_Add_Signature.png");
+const geconAddSignatureSign = require("../../assets/static/Gecon_Add_Signature_Sign.png");
 const keradent = require("../../assets/static/Keradent.jpg");
 const baluarteck = require("../../assets/static/Baluartteck.jpg");
 const saintscript = require("../../assets/static/Saintscript.jpg");
@@ -83,35 +91,129 @@ const imageGecon: Image[] = [{
     route: geconContractList,
     imageTitle: 'Contract List',
     description: ['See all contracts and also update it and delete it.', 'Also you can search any contract, writing the name, DNI, contract folio and number employee.'],
-    listImage: []
+    listImage: [{
+        id: 0,
+        route: geconContractList,
+        imageTitle: '',
+        description: [],
+        listImage: []
+    },
+    {
+        id: 1,
+        route: geconContractInfo,
+        imageTitle: '',
+        description: [],
+        listImage: []
+    },]
 },
 {
     id: 2,
     route: geconEmployeeList,
     imageTitle: 'Employee List',
     description: [' Control the staff and signatories and see you partial information. Also you can search to employee or signers by name, DNI and number of employee. Control the staff and signatories and see you partial information.', 'Also you can search to employee or signers by name, DNI and number of employee.'],
-    listImage: []
+    listImage: [
+        {
+            id: 0,
+            route: geconEmployeeList,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 1,
+            route: geconPersonalInfo,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 2,
+            route: geconPersonalAddress,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+    ]
 },
 {
     id: 3,
     route: geconGenerateContracts,
     imageTitle: 'Generate Contracts',
     description: ['Generate multiple contracts by government dependency, choose contract duration and then choose the signatories (Max. 3 and Min. 2 depending on government dependency) and finally choose staff to generate contract or contracts.', 'This process save it for choose another government dependency and generate multiple contracts to one click.'],
-    listImage: []
+    listImage: [
+        {
+            id: 0,
+            route: geconGenerateContracts,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 1,
+            route: geconGenerateContracts2,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 2,
+            route: geconGenerateContracts3,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+    ]
 },
 {
     id: 4,
     route: geconUpdateContract,
     imageTitle: 'Update Contract',
     description: ['Update contracts, change the duration, government dependency or position as long as the contract has not been signed.'],
-    listImage: []
+    listImage: [
+        {
+            id: 0,
+            route: geconUpdateContract,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 1,
+            route: geconUpdateContractDependencia,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 2,
+            route: geconUpdateContractPuesto,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+    ]
 },
 {
     id: 5,
     route: geconAddSignature,
     imageTitle: 'Add signers',
     description: ['Add new signers, choose by government dependency, then write or choose you position and indicate the order to sig.'],
-    listImage: []
+    listImage: [
+        {
+            id: 0,
+            route: geconAddSignature,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 1,
+            route: geconAddSignatureSign,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+    ]
 }
 ]
 
