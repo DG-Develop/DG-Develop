4ff65069ecd881d5318c8d8f2b136c822cfe6938
DG-Develop <dgdband@gmail.com>
2022-12-06 23:28:16 -0600

    [ADD] Agregando más información sobre el trabajo de keradent

--- src/data/KeradentMockImage.ts
+++ src/data/KeradentMockImage.ts
@@ -9,6 +9,12 @@ const keradentPatientExpedient = require("../assets/static/Keradent_Patient_Expe
 const keradentPatientExpedient2 = require("../assets/static/Keradent_Patient_Expedient2.png");
 const keradentIntraoral = require("../assets/static/Keradent_Intraoral.png");
 const keradentIntraoral2 = require("../assets/static/Keradent_Intraoral2.png");
+const keradentPast = require("../assets/static/Keradent_Past.png");
+const keradentPast2 = require("../assets/static/Keradent_Past2.png");
+const keradentOdontogram = require("../assets/static/Keradent_Odontogram.png");
+const keradentOdontogram2 = require("../assets/static/Keradent_Odontogram2.png");
+const keradentOdontogram3 = require("../assets/static/Keradent_Odontogram3.png");
+const keradentOdontogram4 = require("../assets/static/Keradent_Odontogram4.png");
 
 const imageKeradent: Image[] = [
     {
@@ -103,6 +109,62 @@ const imageKeradent: Image[] = [
         {
             id: 1,
             route: keradentIntraoral2,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        ]
+    },
+    {
+        id: 4,
+        route: keradentPast,
+        imageTitle: 'Personal history and illnesses',
+        description: ['Select your personal history or illnesses of the patient.', 'Add other illnesses if do not appear in the list. asdfasdfsdfadfaassd'],
+        listImage: [{
+            id: 0,
+            route: keradentPast,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 1,
+            route: keradentPast2,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        ]
+    },
+    {
+        id: 4,
+        route: keradentOdontogram,
+        imageTitle: 'Odontogram',
+        description: ['Select the odontogram between adult or child.', 'Select patient affected areas, and write some observation.'],
+        listImage: [{
+            id: 0,
+            route: keradentOdontogram,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 1,
+            route: keradentOdontogram2,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 2,
+            route: keradentOdontogram3,
+            imageTitle: '',
+            description: [],
+            listImage: []
+        },
+        {
+            id: 3,
+            route: keradentOdontogram4,
             imageTitle: '',
             description: [],
             listImage: []
