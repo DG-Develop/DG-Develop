8212878cb223eea6d7bf1e555f33bfa4825a6f31
DG-Develop <dgdband@gmail.com>
2022-12-25 15:05:21 -0600

    [ADD] Agregando informacion al proyecto de baluarteck para desktop

--- src/data/BaluarteckMockImage.ts
+++ src/data/BaluarteckMockImage.ts
@@ -14,6 +14,32 @@ const baluarteckConfig2 = require("../assets/static/Baluarteck_Config2.png");
 const baluarteckConfig3 = require("../assets/static/Baluarteck_Config3.png");
 const baluarteckConfig4 = require("../assets/static/Baluarteck_Config4.png");
 const baluarteckDesktop = require("../assets/static/Baluarteck_Desktop.png");
+const baluarteckDesktopReport = require("../assets/static/Baluarteck_Desktop_Report.png");
+const baluarteckDesktopReport2 = require("../assets/static/Baluarteck_Desktop_Report2.png");
+const baluarteckDesktopReport3 = require("../assets/static/Baluarteck_Desktop_Report3.png");
+const baluarteckDesktopReport4 = require("../assets/static/Baluarteck_Desktop_Report4.png");
+const baluarteckDesktopReport5 = require("../assets/static/Baluarteck_Desktop_Report5.png");
+const baluarteckDesktopReport6 = require("../assets/static/Baluarteck_Desktop_Report6.png");
+const baluarteckDesktopReport7 = require("../assets/static/Baluarteck_Desktop_Report7.png");
+const baluarteckDesktopReport8 = require("../assets/static/Baluarteck_Desktop_Report8.png");
+const baluarteckDesktopIncidents = require("../assets/static/Baluarteck_Desktop_Incidents.png");
+const baluarteckDesktopIncidents2 = require("../assets/static/Baluarteck_Desktop_Incidents2.png");
+const baluarteckDesktopIncidents3 = require("../assets/static/Baluarteck_Desktop_Incidents3.png");
+const baluarteckDesktopIncidents4 = require("../assets/static/Baluarteck_Desktop_Incidents4.png");
+const baluarteckDesktopIncidents5 = require("../assets/static/Baluarteck_Desktop_Incidents5.png");
+const baluarteckDesktopIncidents6 = require("../assets/static/Baluarteck_Desktop_Incidents6.png");
+const baluarteckDesktopEmployee = require("../assets/static/Baluarteck_Desktop_Employees.png");
+const baluarteckDesktopEmployee2 = require("../assets/static/Baluarteck_Desktop_Employees2.png");
+const baluarteckDesktopEmployee3 = require("../assets/static/Baluarteck_Desktop_Employees3.png");
+const baluarteckDesktopEmployee4 = require("../assets/static/Baluarteck_Desktop_Employees4.png");
+const baluarteckDesktopEmployee5 = require("../assets/static/Baluarteck_Desktop_Employees5.png");
+const baluarteckDesktopEmployee6 = require("../assets/static/Baluarteck_Desktop_Employees6.png");
+const baluarteckDesktopEmployee7 = require("../assets/static/Baluarteck_Desktop_Employees7.png");
+const baluarteckDesktopEmployee8 = require("../assets/static/Baluarteck_Desktop_Employees8.png");
+const baluarteckDesktopEmployee9 = require("../assets/static/Baluarteck_Desktop_Employees9.png");
+const baluarteckDesktopEmployee10 = require("../assets/static/Baluarteck_Desktop_Employees10.png");
+const baluarteckDesktopEmployee11 = require("../assets/static/Baluarteck_Desktop_Employees11.png");
+const baluarteckDesktopEmployee12 = require("../assets/static/Baluarteck_Desktop_Employees12.png");
 
 
 const imageBaluarteck: Image[] = [{
@@ -174,6 +200,241 @@ const imageBaluarteck: Image[] = [{
         {
             id: 0,
             route: baluarteckDesktop,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+    ],
+    media: 'desktop'
+},
+{
+    id: 5,
+    route: baluarteckDesktopReport,
+    imageTitle: 'Create & Generate Reports',
+    description: ["See a list of reports and create a report taking into account absences or not, or if you want both, additionally you can include additional data into the report and choose between incidents with pay or without pay, or simply choose both.", "Generate report choosing intial date and finish date, select all employees or some employees, filter by job or profile. Then you see a report with the add caracteristics that you choosed before."],
+    listImage: [
+        {
+            id: 0,
+            route: baluarteckDesktopReport,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 1,
+            route: baluarteckDesktopReport2,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 2,
+            route: baluarteckDesktopReport3,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 3,
+            route: baluarteckDesktopReport4,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 4,
+            route: baluarteckDesktopReport5,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 5,
+            route: baluarteckDesktopReport6,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 6,
+            route: baluarteckDesktopReport7,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 7,
+            route: baluarteckDesktopReport8,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+    ],
+    media: 'desktop'
+},
+{
+    id: 6,
+    route: baluarteckDesktopIncidents,
+    imageTitle: 'Create Incidents',
+    description: ["Create incidents of your employees with paid or unpaid.", "Fill the form, write de motive of the incident, fill the initial and finish date and the staff name with authorize, and finally choose the permit type.", "See the information about incident it and look the missing days for that incident finish."],
+    listImage: [
+        {
+            id: 0,
+            route: baluarteckDesktopIncidents,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 1,
+            route: baluarteckDesktopIncidents2,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 2,
+            route: baluarteckDesktopIncidents3,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 3,
+            route: baluarteckDesktopIncidents4,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 4,
+            route: baluarteckDesktopIncidents5,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 5,
+            route: baluarteckDesktopIncidents6,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+    ],
+    media: 'desktop'
+},
+{
+    id: 6,
+    route: baluarteckDesktopEmployee,
+    imageTitle: 'Employee Control',
+    description: ["Register, input check-in and look the list of all employees.", "Additionally you can see the information of the employee, update your information, look employee schedule, look employee incidents and look the list of check-in ."],
+    listImage: [
+        {
+            id: 0,
+            route: baluarteckDesktopEmployee,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 1,
+            route: baluarteckDesktopEmployee2,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 2,
+            route: baluarteckDesktopEmployee3,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 3,
+            route: baluarteckDesktopEmployee4,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 4,
+            route: baluarteckDesktopEmployee5,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 5,
+            route: baluarteckDesktopEmployee6,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 6,
+            route: baluarteckDesktopEmployee7,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 7,
+            route: baluarteckDesktopEmployee8,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 8,
+            route: baluarteckDesktopEmployee9,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 9,
+            route: baluarteckDesktopEmployee10,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 10,
+            route: baluarteckDesktopEmployee11,
+            imageTitle: '',
+            description: [],
+            listImage: [],
+            media: 'desktop'
+        },
+        {
+            id: 11,
+            route: baluarteckDesktopEmployee12,
             imageTitle: '',
             description: [],
             listImage: [],
